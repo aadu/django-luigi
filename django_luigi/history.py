2595d7131f80f57a49e34633ff26870534055be2
Aaron Duke <dukuru@gmail.com>
2018-09-19 14:30:14 -0700

    history fixes

--- django_luigi/history.py
+++ django_luigi/history.py
@@ -1,34 +1,20 @@
-from django.db import models
+import os
+import sys
 
-
-class TableUpdates(models.Model):
-    update_id = models.CharField(max_length=128, null=False, primary_key=True)
-    target_model = models.CharField(max_length=128)
-    inserted = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = 'table_updates'
-
+import django
+from django.db import models, transaction
+from django.apps import apps
+from django.conf import settings
 import datetime
 import logging
 from contextlib import contextmanager
-from django.apps import apps
-from django.conf import settings
 from luigi import six
-
 from luigi import configuration
 from luigi import task_history
 from luigi.task_status import DONE, FAILED, PENDING, RUNNING
 
-import sqlalchemy
-import sqlalchemy.ext.declarative
-import sqlalchemy.orm
-import sqlalchemy.orm.collections
-from sqlalchemy.engine import reflection
-
+from .models import TaskRecord, TaskEvent, TaskParameter
 logger = logging.getLogger('django-luigi')
-
-from . import models
 
 
 class DjangoTaskHistory(task_history.TaskHistory):
@@ -38,34 +24,16 @@ class DjangoTaskHistory(task_history.TaskHistory):
     """
     CURRENT_SOURCE_VERSION = 1
 
-    def setup_django(self):
-        if not apps.ready and not settings.configured:
-            sys.path.append(self.django_root)
-            os.environ.setdefault("DJANGO_SETTINGS_MODULE", self.settings_path)
-            django.setup()
-
-        self.marker_model_bound = apps.get_model(
-            'django_luigi', self.marker_model)
-
-        if self.marker_model is None:
-            self.marker_model = luigi.configuration.get_config().get(
-                'django', 'marker-model', 'TableUpdates')
-        if not apps.ready and not settings.configured:
-
     def __init__(self):
         config = configuration.get_config()
-        connection_string = config.get('task_history', 'db_connection')
-        self.engine = sqlalchemy.create_engine(connection_string)
-        self.session_factory = sqlalchemy.orm.sessionmaker(
-            bind=self.engine, expire_on_commit=False)
-        Base.metadata.create_all(self.engine)
+        self.django_root = config.get('task_history', 'django_root')
+        self.django_settings_module = config.get('task_history', 'django_settings_module')
+        self.setup_django()
         self.tasks = {}  # task_id -> TaskRecord
-
-        _upgrade_schema(self.engine)
 
     def task_scheduled(self, task):
         htask = self._get_task(task, status=PENDING)
-        event = models.TaskEvent(
+        event = TaskEvent(
             event_name=PENDING,
             ts=datetime.datetime.now(),
         )
@@ -74,7 +42,7 @@ class DjangoTaskHistory(task_history.TaskHistory):
     def task_finished(self, task, successful):
         event_name = DONE if successful else FAILED
         htask = self._get_task(task, status=event_name)
-        event = models.TaskEvent(
+        event = TaskEvent(
             event_name=event_name,
             ts=datetime.datetime.now(),
         )
@@ -82,7 +50,7 @@ class DjangoTaskHistory(task_history.TaskHistory):
 
     def task_started(self, task, worker_host):
         htask = self._get_task(task, status=RUNNING, host=worker_host)
-        event = models.TaskEvent(
+        event = TaskEvent(
             event_name=RUNNING,
             ts=datetime.datetime.now(),
         )
@@ -104,82 +72,97 @@ class DjangoTaskHistory(task_history.TaskHistory):
             event.task = task_record
             event.save()
 
+
+    @transaction.atomic()
     def _find_or_create_task(self, task):
         if task.record_id is not None:
             logger.debug(
                 "Finding task with record_id [%d]", task.record_id)
-            task_record = models.TaskRecord.objects.get(pk=task.record_id)
-            if not task_record:
-                raise Exception(
-                    "Task with record_id, but no matching Task record!")
+            try:
+                task_record = TaskRecord.objects.get(pk=task.record_id)
+            except TaskRecord.ObjectDoesNotExist:
+                raise Exception("Task with record_id, but no matching Task record!")
+            if task.host and task.host != task_record.host:
+                task_record.host = task.host
+                task_record.save()
             yield task_record
         else:
-            task_record = models.TaskRecord(
+            task_record = TaskRecord.objects.create(
                 id=task._task.id, name=task.task_family, host=task.host)
-            for (k, v) in six.iteritems(task.parameters):
-                task_record.parameters[k] = TaskParameter(name=k, value=v)
-            session.add(task_record)
-            yield (task_record, session)
-        if task.host:
-            task_record.host = task.host
+            for k, v in task.parameters.items():
+                TaskParameter.objects.create(
+                    task=task_record, name=k, value=v
+                )
+            yield task_record
         task.record_id = task_record.id
 
-    def find_all_by_parameters(self, task_name, session=None, **task_params):
-        """
-        Find tasks with the given task_name and the same parameters as the kwargs.
-        """
-        with self._session(session) as session:
-            query = session.query(TaskRecord).join(
-                TaskEvent).filter(TaskRecord.name == task_name)
-            for (k, v) in six.iteritems(task_params):
-                alias = sqlalchemy.orm.aliased(TaskParameter)
-                query = query.join(alias).filter(
-                    alias.name == k, alias.value == v)
+    def setup_django(self):
+        if not apps.ready and not settings.configured:
+            sys.path.append(self.django_root)
+            os.environ.setdefault(
+                "DJANGO_SETTINGS_MODULE", self.django_settings_module)
+            django.setup()
 
-            tasks = query.order_by(TaskEvent.ts)
-            for task in tasks:
-                # Sanity check
-                assert all(k in task.parameters and v == str(
-                    task.parameters[k].value) for (k, v) in six.iteritems(task_params))
+    # def find_all_by_parameters(self, task_name, session=None, **task_params):
+    #     """
+    #     Find tasks with the given task_name and the same parameters as the kwargs.
+    #     """
+    #     qs = TaskEvent.objects.filter(name=task_name, **task_params).prefetch_related('parameters')
+    #     for k, v in task_params.items():
+    #         qs = qs.annotate(**{k})
 
-                yield task
+    #     with self._session(session) as session:
+    #         query = session.query(TaskRecord).join(
+    #             TaskEvent).filter(TaskRecord.name == task_name)
+    #         for (k, v) in six.iteritems(task_params):
+    #             alias = sqlalchemy.orm.aliased(TaskParameter)
+    #             query = query.join(alias).filter(
+    #                 alias.name == k, alias.value == v)
 
-    def find_all_by_name(self, task_name, session=None):
-        """
-        Find all tasks with the given task_name.
-        """
-        return self.find_all_by_parameters(task_name, session)
+    #         tasks = query.order_by(TaskEvent.ts)
+    #         for task in tasks:
+    #             # Sanity check
+    #             assert all(k in task.parameters and v == str(
+    #                 task.parameters[k].value) for (k, v) in six.iteritems(task_params))
 
-    def find_latest_runs(self, session=None):
-        """
-        Return tasks that have been updated in the past 24 hours.
-        """
-        with self._session(session) as session:
-            yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-            return session.query(TaskRecord).\
-                join(TaskEvent).\
-                filter(TaskEvent.ts >= yesterday).\
-                group_by(TaskRecord.id, TaskEvent.event_name, TaskEvent.ts).\
-                order_by(TaskEvent.ts.desc()).\
-                all()
+    #             yield task
 
-    def find_all_runs(self, session=None):
-        """
-        Return all tasks that have been updated.
-        """
-        with self._session(session) as session:
-            return session.query(TaskRecord).all()
+    # def find_all_by_name(self, task_name, session=None):
+    #     """
+    #     Find all tasks with the given task_name.
+    #     """
+    #     return self.find_all_by_parameters(task_name, session)
 
-    def find_all_events(self, session=None):
-        """
-        Return all running/failed/done events.
-        """
-        with self._session(session) as session:
-            return session.query(TaskEvent).all()
+    # def find_latest_runs(self, session=None):
+    #     """
+    #     Return tasks that have been updated in the past 24 hours.
+    #     """
+    #     with self._session(session) as session:
+    #         yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    #         return session.query(TaskRecord).\
+    #             join(TaskEvent).\
+    #             filter(TaskEvent.ts >= yesterday).\
+    #             group_by(TaskRecord.id, TaskEvent.event_name, TaskEvent.ts).\
+    #             order_by(TaskEvent.ts.desc()).\
+    #             all()
 
-    def find_task_by_id(self, id, session=None):
-        """
-        Find task with the given record ID.
-        """
-        with self._session(session) as session:
-            return session.query(TaskRecord).get(id)
+    # def find_all_runs(self, session=None):
+    #     """
+    #     Return all tasks that have been updated.
+    #     """
+    #     with self._session(session) as session:
+    #         return session.query(TaskRecord).all()
+
+    # def find_all_events(self, session=None):
+    #     """
+    #     Return all running/failed/done events.
+    #     """
+    #     with self._session(session) as session:
+    #         return session.query(TaskEvent).all()
+
+    # def find_task_by_id(self, id, session=None):
+    #     """
+    #     Find task with the given record ID.
+    #     """
+    #     with self._session(session) as session:
+    #         return session.query(TaskRecord).get(id)
