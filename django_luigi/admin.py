024090e29684c22fcdcda4afa31b08616c9db729
Aaron Duke <dukuru@gmail.com>
2018-09-20 13:49:35 -0700

    Model updates

--- django_luigi/admin.py
+++ django_luigi/admin.py
@@ -1,6 +1,48 @@
 from django.contrib import admin
 from django_luigi.models import TaskRecord, TaskEvent, TaskParameter
 
-admin.site.register(TaskRecord)
-admin.site.register(TaskEvent)
-admin.site.register(TaskParameter)
+
+class ReadOnlyAdminMixin:
+    min_num = 0
+    can_delete = False
+
+    def get_readonly_fields(self, request, obj=None):
+        return self.fields or [f.name for f in self.model._meta.fields]
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class InlineTaskEventAdmin(ReadOnlyAdminMixin, admin.TabularInline):
+    show_change_link = True
+    model = TaskEvent
+
+
+class InlineTaskParameterAdmin(ReadOnlyAdminMixin, admin.TabularInline):
+    model = TaskParameter
+
+
+@admin.register(TaskRecord)
+class TaskRecordAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    # readonly_fields = ('name', 'namespace', 'host')
+    list_filter = ('name', 'namespace')
+    list_display = ('task_id', 'name', 'namespace', 'host',)
+    inlines = [
+        InlineTaskParameterAdmin,
+        InlineTaskEventAdmin,
+    ]
+
+
+@admin.register(TaskEvent)
+class TaskEventAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ('task', 'name', 'ts')
+    list_filter = ('name', 'ts')
+    ordering = ['ts']
+
+
--- django_luigi/models.py
+++ django_luigi/models.py
@@ -1,6 +1,12 @@
-from django.db import models
+from django.db import models, transaction
 from django.dispatch import receiver
+from django.utils import timezone
 from . import signals
+
+class Status:
+    START = 'START'
+    FAILURE = 'FAILURE'
+    SUCCESS = 'SUCCESS'
 
 
 class TableUpdates(models.Model):
@@ -11,21 +17,22 @@ class TableUpdates(models.Model):
     class Meta:
         db_table = 'table_updates'
 
-
 class TaskRecord(models.Model):
     """
     Base table to track information about a luigi.Task.
     References to other tables are available through task.events, task.parameters, etc.
     """
-    task_id = models.CharField(max_length=200, db_index=True)
-    name = models.CharField(max_length=128, db_index=True)
-    host = models.CharField(max_length=128)
+    task_id = models.CharField(
+        max_length=200, db_index=True, unique=True, editable=False)
+    name = models.CharField(max_length=128, db_index=True, editable=False)
+    namespace = models.CharField(max_length=128, null=True, editable=False)
+    host = models.CharField(max_length=128, null=True, editable=False)
 
     class Meta:
         db_table = 'tasks'
         default_related_name = 'tasks'
 
-    def __repr__(self):
+    def __str__(self):
         return "TaskRecord(name=%s, host=%s)" % (self.name, self.host)
 
 
@@ -33,11 +40,11 @@ class TaskParameter(models.Model):
     """
     Table to track luigi.Parameter()s of a Task.
     """
-    task = models.ForeignKey(TaskRecord, on_delete=models.CASCADE)
-    name = models.CharField(max_length=128, db_index=True)
-    value = models.TextField()
+    task = models.ForeignKey('TaskRecord', on_delete=models.CASCADE, editable=False)
+    name = models.CharField(max_length=128, db_index=True, editable=False)
+    value = models.TextField(null=True, editable=False)
 
-    def __repr__(self):
+    def __str__(self):
         return "TaskParameter(task_id=%d, name=%s, value=%s)" % (self.task_id, self.name, self.value)
 
     class Meta:
@@ -50,19 +57,52 @@ class TaskEvent(models.Model):
     """
     Table to track when a task is scheduled, starts, finishes, and fails.
     """
-    task = models.ForeignKey(TaskRecord, on_delete=models.CASCADE)
-    event_name = models.CharField(max_length=20)
-    ts = models.DateTimeField(blank=True, null=True, db_index=True)
+    task = models.ForeignKey('TaskRecord', on_delete=models.CASCADE, editable=False)
+    name = models.CharField(max_length=24, editable=False)
+    ts = models.DateTimeField(db_index=True, editable=False, auto_now_add=True)
 
-    def __repr__(self):
-        return "TaskEvent(task_id=%s, event_name=%s, ts=%s" % (self.task_id, self.event_name, self.ts)
+    def __str__(self):
+        return "TaskEvent(task_id=%s, name=%s, ts=%s" % (self.task_id, self.name, self.ts)
 
     class Meta:
         db_table = 'task_events'
         default_related_name = 'events'
 
 
+
+@transaction.atomic()
+def register_task_event(task, event, **kwargs):
+    defaults = {
+        'name': task.get_task_family(),
+        'namespace': task.get_task_namespace(),
+    }
+    task_record, _ = TaskRecord.objects.update_or_create(
+        task_id=task.task_id,
+        defaults=defaults,
+    )
+    for key, value in task.param_kwargs.items():
+        TaskParameter.objects.get_or_create(
+            task=task_record,
+            name=key,
+            value=value,
+        )
+    TaskEvent.objects.create(
+        task=task_record,
+        name=event,
+        ts=timezone.now(),
+    )
+
+
 @receiver(signals.start)
-def my_callback(sender, task, **kwargs):
-    print(task)
-    print("Request finished!")
+def register_task_start(sender, task, **kwargs):
+    register_task_event(task, Status.START)
+
+
+@receiver(signals.failure)
+def register_task_failure(sender, task, **kwargs):
+    register_task_event(task, Status.FAILURE)
+
+
+@receiver(signals.success)
+def register_task_success(sender, task, **kwargs):
+    register_task_event(task, Status.SUCCESS)
